1261a4a46c83184af9eb41d24ef13967b11489f2
v4rts <v4rts@yandex.ru>
2020-11-21 23:21:27 +0300

    propfession fixed

--- send.php
+++ send.php
@@ -1,26 +1,26 @@
 <?php
 
-	$profession = $_POST['profession'];
+	$proffession = $_POST['proffession'];
 	$exams = $_POST['exams'];
 	$events = $_POST['events'];
 
 
-	$profession = htmlspecialchars($profession);
+	$proffession = htmlspecialchars($proffession);
 	$exams = htmlspecialchars($exams);
 	$events = htmlspecialchars($events);
 
-	$profession = urldecode($profession);
+	$proffession = urldecode($proffession);
 	$exams = urldecode($exams);
 	$events = urldecode($events);
 
-	$profession = trim($profession);
+	$proffession = trim($proffession);
 	$exams = trim($exams);
 	$events = trim($events);
 
 
 	if (mail("v4rts@yandex.ru",
 			 "Новая анкета!",
-			 "Профессия: ".$profession."\n".
+			 "Профессия: ".$proffession."\n".
 			 "Экзамены: ".$exams."\n".
 			 "Мероприятия: ".$events,
 			 "From: no-reply@mydomain \r\n")
